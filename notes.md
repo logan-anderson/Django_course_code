a3b55eedcc4ac09338ea47e27961c684cc2c045d
logan anderson <43075109+yologan55@users.noreply.github.com>
2019-03-26 12:12:59 -0300

    added listing and realtors apps

--- notes.md
+++ notes.md
@@ -1,7 +1,7 @@
 #notes
 ###### commands
     *  python manage.py runserver  (run server)
-    *  python manage.py startapp pages (create an app)
+    *  python manage.py startapp pages (create an app called pages)
     *  
     
     
--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'pages/index.html')
+    return render(request, 'pages/index.html')
+
 
 def about(request):
     return render(request, 'pages/about.html')
