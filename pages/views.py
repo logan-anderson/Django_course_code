6caa8914d6243de4eba9202bc5d436db3f90b419
logan anderson <43075109+yologan55@users.noreply.github.com>
2019-04-22 15:07:29 -0300

    worked on course

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 # Create your views here.
 
 def index(request):
@@ -10,6 +11,9 @@ def index(request):
 
     context = {
         'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
     return render(request, 'pages/index.html', context)
 
