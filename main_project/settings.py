e4fab6efca15fbfd48c86ae70d1221979a98cff2
logan anderson <43075109+yologan55@users.noreply.github.com>
2019-04-26 14:54:28 -0300

    changed settings.py

--- main_project/settings.py
+++ main_project/settings.py
@@ -146,12 +146,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thegoofymango@gmail.com'
-EMAIL_HOST_PASSWORD = '***'
+
 
 try:
     from .local_settings import *
